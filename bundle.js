41a4a9362707c690c409fa4228ee76708f5b250d
Francesco (makevoid) <makevoid@gmail.com>
2015-04-27 00:42:55 +0100

    update coffee

--- bundle.js
+++ bundle.js
@@ -1,13 +1,17 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-var unique = require('uniq')
-//var unique = require('./node_modules/uniq')
+var data, output, unique;
 
-var data = [1, 2, 2, 3, 4, 5, 5, 5, 6]
+unique = require('uniq');
 
-var output = unique(data)
+data = [1, 2, 2, 3, 4, 5, 5, 5, 6];
 
-console.log(output)
-document.querySelector("#output").innerHTML = output
+output = unique(data);
+
+console.log(output);
+
+document.querySelector("#output").innerHTML = output;
+
+
 
 },{"uniq":2}],2:[function(require,module,exports){
 "use strict"
--- index.js
+++
@@ -1,9 +0,0 @@
-var unique = require('uniq')
-//var unique = require('./node_modules/uniq')
-
-var data = [1, 2, 2, 3, 4, 5, 5, 5, 6]
-
-var output = unique(data)
-
-console.log(output)
-document.querySelector("#output").innerHTML = output
